3b29cf15f92dbf5fd77b13abcdf20d6c6919c262
Brody Over <10548119+brody192@users.noreply.github.com>
2025-10-29 10:45:19 -0400

    Updates Tailscale dependency version and fixes idle connections. (#9)

--- tcp.go
+++ tcp.go
@@ -3,15 +3,22 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"net"
+	"time"
 
+	"github.com/half0wl/railtail/internal/util"
+	"github.com/northbright/iocopy"
 	"golang.org/x/sync/errgroup"
 	"tailscale.com/tsnet"
 )
 
 func fwdTCP(lstConn net.Conn, ts *tsnet.Server, targetAddr string) error {
 	defer lstConn.Close()
+
+	if tcpConn, ok := lstConn.(*net.TCPConn); ok {
+		tcpConn.SetKeepAlive(true)
+		tcpConn.SetKeepAlivePeriod(30 * time.Second)
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -23,19 +30,40 @@ func fwdTCP(lstConn net.Conn, ts *tsnet.Server, targetAddr string) error {
 
 	defer tsConn.Close()
 
+	if tcpConn, ok := tsConn.(*net.TCPConn); ok {
+		tcpConn.SetKeepAlive(true)
+		tcpConn.SetKeepAlivePeriod(30 * time.Second)
+	}
+
 	g, ctx := errgroup.WithContext(ctx)
 
 	g.Go(func() error {
-		if _, err := io.Copy(tsConn, lstConn); err != nil {
-			return fmt.Errorf("failed to copy data to tailscale node: %w", err)
+		defer cancel()
+
+		defer func() {
+			if tcpConn, ok := tsConn.(*net.TCPConn); ok {
+				tcpConn.CloseWrite()
+			}
+		}()
+
+		if _, err := iocopy.Copy(ctx, tsConn, lstConn); err != nil && !util.IsExpectedCopyError(err) {
+			return fmt.Errorf("failed to copy data to target: %w", err)
 		}
 
 		return nil
 	})
 
 	g.Go(func() error {
-		if _, err := io.Copy(lstConn, tsConn); err != nil {
-			return fmt.Errorf("failed to copy data from tailscale node: %w", err)
+		defer cancel()
+
+		defer func() {
+			if tcpConn, ok := lstConn.(*net.TCPConn); ok {
+				tcpConn.CloseWrite()
+			}
+		}()
+
+		if _, err := iocopy.Copy(ctx, lstConn, tsConn); err != nil && !util.IsExpectedCopyError(err) {
+			return fmt.Errorf("failed to copy data from source: %w", err)
 		}
 
 		return nil
